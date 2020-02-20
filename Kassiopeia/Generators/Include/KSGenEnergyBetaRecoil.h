2395ee492177190b421a6027c361332f3f671c11
Wouter Deconinck <wdconinc@gmail.com>
2020-02-20 14:12:05 -0600

    Neutron beta decay recoil energy generator
    This generator, used with the tag `energy_beta_recoil` as in
    ```
        <ksgen_generator_composite name="generator_uniform" pid="2212">
            <energy_beta_recoil/>
        </ksgen_generator_composite>
    ```
    will generate an initial kinetic energy per the energy distribution
    of the recoiling proton in beta decay.
    
    The implemented expression is that of eqns 2.38ff in the PhD thesis
    of Gertrud Emilie Konrad, JHU Mainz, available at
    http://inspirehep.net/record/1653653/files/3053.pdf
    
    No attempt has been made to support nuclear beta decay recoil for
    anything other than simple neutron decay.
    
    No Coulomb corrections or radiative corrections are included.

---
+++ Kassiopeia/Generators/Include/KSGenEnergyBetaRecoil.h
@@ -0,0 +1,59 @@
+//
+// Created by wdconinc on 13.02.20.
+//
+
+#ifndef KASPER_KSGENENERGYBETARECOIL_H
+#define KASPER_KSGENENERGYBETARECOIL_H
+
+#include "KSGenCreator.h"
+#include "KField.h"
+
+namespace Kassiopeia
+{
+    class KSGenEnergyBetaRecoil:
+            public KSComponentTemplate< KSGenEnergyBetaRecoil, KSGenCreator >
+    {
+    public:
+        KSGenEnergyBetaRecoil();
+        KSGenEnergyBetaRecoil( const KSGenEnergyBetaRecoil& aCopy );
+        KSGenEnergyBetaRecoil* Clone() const;
+        virtual ~KSGenEnergyBetaRecoil();
+
+        //******
+        //action
+        //******
+
+    public:
+        void Dice( KSParticleQueue* aPrimaries );
+
+        //*************
+        //configuration
+        //*************
+
+    public:
+        double g(double E);
+        double g1(double E);
+        double g2(double E);
+        double GetRecoilEnergyMax();
+        double GetRecoilEnergyProbabilityMax(double Emax);
+        double GenRecoilEnergy();
+
+    private:
+        ;K_SET_GET( int, NMax );
+        ;K_SET_GET( double, EMax );
+        ;K_SET_GET( double, PMax );
+        ;K_SET_GET( double, MinEnergy );
+        ;K_SET_GET( double, MaxEnergy );
+
+        //**********
+        //initialize
+        //**********
+
+    public:
+        void InitializeComponent();
+        void DeinitializeComponent();
+    };
+
+}
+
+#endif //KASPER_KSGENENERGYBETARECOIL_H
