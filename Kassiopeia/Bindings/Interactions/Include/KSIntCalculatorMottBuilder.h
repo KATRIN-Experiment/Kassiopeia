9448d42dc1033f55b7ff8588aadd352cf2787249
juniorpena <72262395+juniorpena@users.noreply.github.com>
2024-05-06 15:10:58 -0700

    Update KSIntCalculatorMottBuilder.h

--- Kassiopeia/Bindings/Interactions/Include/KSIntCalculatorMottBuilder.h
+++ Kassiopeia/Bindings/Interactions/Include/KSIntCalculatorMottBuilder.h
@@ -18,11 +18,41 @@ template<> inline bool KSIntCalculatorMottBuilder::AddAttribute(KContainer* aCon
     }
     if (aContainer->GetName() == "theta_min") {
         aContainer->CopyTo(fObject, &KSIntCalculatorMott::SetThetaMin);
-        return true;
+
+        if ((fObject->GetThetaMin() == 0.0)) {
+            initmsg(eError) << "\"" << fObject->GetThetaMin()
+                           << R"(" is an invalid lower bound for Mott scattering! Change to a value > 0)" << eom;
+
+            return false;
+        }
+        else {
+            return true;
+        }
     }
     if (aContainer->GetName() == "theta_max") {
         aContainer->CopyTo(fObject, &KSIntCalculatorMott::SetThetaMax);
-        return true;
+        if ((fObject->GetThetaMax() > 180.0)) {
+            initmsg(eError) << "\"" << fObject->GetThetaMax()
+                           << R"(" is an invalid upper bound for Mott scattering! Change to a value < 180)" << eom;
+
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    if (aContainer->GetName() == "nucleus") {
+        aContainer->CopyTo(fObject, &KSIntCalculatorMott::SetNucleus);
+
+        if ((fObject->GetNucleus().compare("He") != 0) && (fObject->GetNucleus().compare("Ne") != 0)) {
+            initmsg(eError) << "\"" << fObject->GetNucleus()
+                           << R"(" is not available for Mott scattering! Available gases: "He", "Ne")" << eom;
+
+            return false;
+        }
+        else {
+            return true;
+        }
     }
     return false;
 }
